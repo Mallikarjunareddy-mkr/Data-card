84771d751a97bf029a9293b6b732ac9487d30290
Malli Karjuna Reddy <141660622+Mallikarjunareddy-mkr@users.noreply.github.com>
2025-09-28 18:43:12 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.routers import notes, files
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+app.include_router(notes.router, prefix="/notes")
+app.include_router(files.router, prefix="/files")
